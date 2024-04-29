2fe4ee0bb22154eaf50dd7c1218d9df285657e4f
Brighty <brightyjnithin@gmail.com>
2024-04-29 11:01:18 +0530

    FRONTEND  COMMITTED

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import './Hero.css';
 import arrow_icon from '../Assets/arrow.png';
-import slide1 from '../Assets/slide1.jpg.jpg';
-import slide2 from '../Assets/slide2.jpg.jpg';
-import slide3 from '../Assets/slide3.png.jpg';
 import slide4 from '../Assets/slide4.png.jpg';
 
 const Hero = () => {
-  // Slider settings
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
-
   return (
     <div className='hero'>
       <div className="hero-left">
@@ -26,24 +11,11 @@ const Hero = () => {
         <p> STOCK</p>
         <div className="hero-latest-btn">
           <div>Latest Greens</div>
-          <img src={arrow_icon} alt=""/>
+          <img src={arrow_icon} alt="Arrow Icon"/>
         </div>
       </div>
       <div className="hero-right">
-        <Slider {...settings}>
-          <div>
-            <img src={slide1} alt="Slide 1"/>
-          </div>
-          <div>
-            <img src={slide2} alt="Slide 2"/>
-          </div>
-          <div>
-            <img src={slide3} alt="Slide 3"/>
-          </div>
-          <div>
-            <img src={slide4} alt="Slide 4"/>
-          </div>
-        </Slider>
+        <img src={slide4} alt="Slide 4"/>
       </div>
     </div>
   );
